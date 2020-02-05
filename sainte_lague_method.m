b99205caf48f4bf06f630223faad60ad5aec2fd0
kittyhawk1980 <60705344+kittyhawk1980@users.noreply.github.com>
2020-02-05 18:05:57 +0100

    Delete sainte_lague_method.m

--- sainte_lague_method.m
+++
@@ -1,9 +0,0 @@
-function new_seats = sainte_lague_method(votes,seats)
-    z = zeros(1,11);
-    for i = 1:11
-        z(i) = votes(i) / (2 * seats(i) + 1);
-    end
-    max_index = find(z == max(z(:)));
-    seats(max_index) = seats(max_index) + 1;
-    new_seats = seats;
-end
